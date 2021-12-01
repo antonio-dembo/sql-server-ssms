549ee330f247cd5178a8a68bdb20c4d0fd2808fa
beboantonio <beboantonio1@hotmail.com>
2021-12-01 08:35:00 -0300

    delete new record

--- demo.sql/Part5-cascading-referential-integrity-constraints.sql
+++ demo.sql/Part5-cascading-referential-integrity-constraints.sql
@@ -1,7 +1,7 @@
 select * from tblGender;
 select * from tblPerson;
 
-delete from tblGender where ID = 1;
+delete from tblGender where ID = 3;
 
 --insert into tblPerson(ID, [Name], Email,GenderID) values (7, 'Patrick', 'p@p.com', 3);
 
